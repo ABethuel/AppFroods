e46160f4e63e20a13d3b7297a94dab5c879cb19e
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-04-06 00:15:29 +0200

    Redux command et améliorations DetailsScreen

--- Screen/DishesScreen/CategorysScreen/DetailScreen.js
+++ Screen/DishesScreen/CategorysScreen/DetailScreen.js
@@ -1,21 +1,112 @@
 // Components/Favorites.js
 
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import HeaderInscription from '../../../Components/HeaderInscription'
+import { connect } from 'react-redux'
 
 class DetailScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dish: undefined,
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      dish: this.props.route.params,
+      isLoading: true
+    })
+  }
+
+  componentDidUpdate() {
+    console.log(this.props.commandMade)
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _displayAddRemoveBtn() {
+    if (this.props.commandMade.findIndex(item => item.idDish === this.state.dish.idDish) !== -1){
+      return(
+        <View>
+          <Text style={styles.text_add}>Annuler la commande</Text>
+        </View>
+      )
+    }
+    else {
+      return(
+        <Text style={styles.text_add}>Commander le plat</Text>
+      )
+    }
+  }
+
+  _displayShowCommand() {
+    if (this.props.commandMade.findIndex(item => item.idDish === this.state.dish.idDish) !== -1){
+      return(
+        <TouchableOpacity 
+          style={styles.btn_show}
+          onPress={() => this.props.navigation.navigate('ActualCommand')}>
+          <Text style={styles.text_add}>Voir la commande</Text>
+        </TouchableOpacity>
+      )
+    }
+  }
+
+  _toggleCommand() {
+    const action = { type: 'TOGGLE_COMMAND', value: this.state.dish}
+    this.props.dispatch(action)
+  }
+
+  _displayDetail() {
+    const {idDish, Name, Date, From, Price, Category, Adress, Description, IMage} = this.props.route.params
+    if (this.state.dish != undefined) {
+      return (
+        <ScrollView>
+          <View style={styles.dish_container}>
+           <Image
+            style={styles.image_dish}
+            source={{uri: IMage}}
+          />
+          <View style={styles.text_container}>
+            <Text style={styles.text_name}>{Name}</Text>
+            <Text style={styles.text_price}>{Price}P</Text>
+            <Text style={styles.text_description}>{Description}</Text>
+            <Text style={styles.text_caracteristics}>Catégorie : {Category}</Text>
+            <Text style={styles.text_caracteristics}>De : {From}</Text>
+            <Text style={styles.text_adress}>Adresse : {Adress}</Text>
+            </View>
+          </View>
+  
+          <TouchableOpacity 
+            style={styles.add_container} 
+            onPress={() => this._toggleCommand()}>
+            {this._displayAddRemoveBtn()}
+          </TouchableOpacity>
+
+          {this._displayShowCommand()}
+  
+        </ScrollView>
+        
+      )
+    }
+  }
 
   render() {
-
-    console.log(this.props.route)
-    const {idDish, Name, Date, From, Price, Category, Adress, Description, IMage} = this.props.route.params
-
-    return (
+    return(
       <View style={styles.main_container}>
         <HeaderInscription/>
-
+  
         <View style={styles.header}>
           <TouchableOpacity onPress={() => this.props.navigation.navigate("Home")}>
             <Image style={styles.image} source={require('../../../images/back.png')}/>
@@ -24,27 +115,8 @@ class DetailScreen extends React.Component {
         </View>
 
         <View style={styles.container}>
-          <ScrollView>
-            <View style={styles.dish_container}>
-              <Image
-                style={styles.image_dish}
-                source={{uri: IMage}}
-              />
-              <View style={styles.text_container}>
-                <Text style={styles.text_name}>{Name}</Text>
-                <Text style={styles.text_price}>{Price}P</Text>
-                <Text style={styles.text_description}>{Description}</Text>
-                <Text style={styles.text_caracteristics}>Catégorie : {Category}</Text>
-                <Text style={styles.text_caracteristics}>De : {From}</Text>
-                <Text style={styles.text_adress}>Adresse : {Adress}</Text>
-              </View>
-            </View>
-
-            <TouchableOpacity style={styles.add_container}>
-              <Text style={styles.text_add}>Commander le plat</Text>
-            </TouchableOpacity>
-
-          </ScrollView>
+          {this._displayLoading()}
+          {this._displayDetail()}
         </View>
       </View>
     )
@@ -55,6 +127,16 @@ const styles = StyleSheet.create({
   main_container:{
     flex:1,
     backgroundColor:'#ECFFFA'
+  },
+
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 
   container:{
@@ -146,8 +228,25 @@ const styles = StyleSheet.create({
     alignItems:'center',
     backgroundColor:'#57B8FF',
     margin: 20,
+    marginTop: 15,
+    marginBottom: 5,
+    borderRadius: 10
+  },
+
+  btn_show:{
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#57B8FF',
+    margin: 20,
+    marginBottom: 25,
     borderRadius: 10
   },
 })
 
-export default DetailScreen
+const mapStateToProps = (state) => {
+  return {
+    commandMade: state.commandMade
+  }
+}
+
+export default connect(mapStateToProps)(DetailScreen)
--- Store/Reducers/commandReducer.js
+++ Store/Reducers/commandReducer.js
@@ -6,8 +6,15 @@ function toggleCommand(state = initialState, action) {
     let nextState
     switch (action.type) {
         case 'TOGGLE_COMMAND':
-            const commandMadeIndex = state.commandMade.findIndex(item => item.id === action.value.id)
-            if (commandMadeIndex === -1) {
+            const commandMadeIndex = state.commandMade.findIndex(item => item.idDish === action.value.idDish)
+            if (commandMadeIndex !== -1 ){
+                // PLat déjà commandé, on l'annule
+                nextState = {
+                    ...state,
+                    commandMade : state.commandMade.filter( (item, index) => index !== commandMadeIndex)
+                }
+            }
+            else{
                 // On ajoute le plat dans la liste des commandes en cours
                 nextState = {
                     ...state,
