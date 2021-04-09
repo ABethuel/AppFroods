ffaa302a7acc33b664a95d71f6bc778089606149
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-04-09 22:30:02 +0200

    Connexion with data

--- Screen/ConnexionScreen.js
+++ Screen/ConnexionScreen.js
@@ -1,15 +1,19 @@
 // Components/Connexion.js
 
 import React from 'react'
-import { View, TextInput, Button , Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator} from 'react-native'
+import { View, TextInput, Button , Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator, Alert} from 'react-native'
 import HeaderInscription from '../Components/HeaderInscription'
+import {getProfil} from '../Data/commandsData'
 
 
 class ConnexionScreen extends React.Component {
+    
     constructor(props) {
         super(props)
         this.state = {
-            isLoading: true
+            isLoading: true,
+            textMail: '',
+            textPassword: '',
         }
     }
 
@@ -23,6 +27,36 @@ class ConnexionScreen extends React.Component {
         }, 1000);
      }
 
+    _getTextMail(text) {
+         this.setState({textMail: text})
+     }
+
+    _getTextPassword(text) {
+        this.setState({textPassword: text})
+    }
+
+    _checkBtn(){
+        const profil = getProfil()
+        if (this.state.textMail.length === 0) {
+            Alert.alert('Veuillez saisir un email');
+        }
+        else if (this.state.textPassword.length === 0) {
+            Alert.alert('Veuillez saisir votre mot de passe');
+        }
+        else if (this.state.textMail !== profil.email) {
+            Alert.alert('Email incorrect !');
+        }
+        else if (this.state.textPassword !== profil.password) {
+            Alert.alert('Mot de passe incorrect !');
+        }
+        else if (this.state.textMail === profil.email && this.state.textPassword === profil.password) {
+            this.props.navigation.navigate('Home')
+        }
+    }
+
+    _test(){
+        Alert.alert()
+    }
     _displayScreen() {
         if (this.state.isLoading === true){
             return(
@@ -39,16 +73,22 @@ class ConnexionScreen extends React.Component {
                     <View style={styles.questionnaire}>
                         <View style={styles.ask}>
                             <Text style={{fontFamily: 'Quicksand-Bold',}}>Email</Text>
-                            <TextInput style={styles.input}/>
+                            <TextInput 
+                                style={styles.input}
+                                onChangeText={(text) => this._getTextMail(text)}
+                                />
                         </View>
                         <View style={styles.ask}>
                             <Text style={{fontFamily: 'Quicksand-Bold',}}>Mot de passe</Text>
-                            <TextInput style={styles.input}/>
+                            <TextInput 
+                                style={styles.input}
+                                onChangeText={(text) => this._getTextPassword(text)}
+                                />
                         </View>
                     </View>
     
                     <View style={styles.btn_connexion} >
-                        <Button color='#57B8FF' title="Se connecter" onPress={() => this.props.navigation.navigate('Home')}/>  
+                        <Button color='#57B8FF' title="Se connecter" onPress={() => this._checkBtn()}/>  
                     </View> 
     
                     <View style={styles.inscription}>
