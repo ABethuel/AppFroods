8a800db4cce25522e359b754cce83d87f74b4067
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-06-28 16:58:33 +0200

    order and proposition entities

---
+++ BackEnd/src/Repository/PropositionRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Proposition;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Proposition|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Proposition|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Proposition[]    findAll()
+ * @method Proposition[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PropositionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Proposition::class);
+    }
+
+    // /**
+    //  * @return Proposition[] Returns an array of Proposition objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Proposition
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
